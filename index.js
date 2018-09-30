75101bb44b6dc6830753598560a0837debc03d07
payomatheo <payomatheo@gmail.com>
2018-09-30 18:52:39 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const colors =require('colors');
+const express = require('express');
+const server = express();
+
+server.get('/',(req,res) => {
+   res.send('<h1>Hola mundo con node y express</h1>');
+   res.end();
+});
+
+server.listen(3000,() => {
+   console.log('Server On port 3000'.green) 
+});
